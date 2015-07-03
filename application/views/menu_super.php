b4554ea0084aa3c08adf1f9278e3e1940ae8ae38
Deddy Rusdiansyah <jokonugroho@Deddy-Rusdiansyah-MacBook-Pro.local>
2015-07-03 14:47:13 +0700

    Sistem Informasi Inventory
    Sistem Informsi Inventory menggunakan codeigniter dan easyui

---
+++ application/views/menu_super.php
@@ -0,0 +1,60 @@
+<div title="Master" data-options="iconCls:'icon-tip'" style="overflow:auto;padding:5px 0px;">
+    <div title="TreeMenu" data-options="iconCls:'icon-search'" style="padding:0px;">
+    <ul class="easyui-tree">
+    <li data-options="iconCls:'icon-surat_perintah'">
+    <a href="<?php echo base_url();?>index.php/pengguna">Pengguna</a>
+    </li>
+    <li data-options="iconCls:'icon-surat_keputusan'">
+    <a href="<?php echo base_url();?>index.php/barang">Barang</a>
+    </li>
+    <li data-options="iconCls:'icon-surat_keluar'">
+    <a href="<?php echo base_url();?>index.php/supplier">Supplier</a>
+    </li>
+    </ul>
+    </div>
+</div>
+<div title="Transaksi" data-options="iconCls:'icon-tip'" style="overflow:auto;padding:5px 0px;">
+    <div title="TreeMenu" data-options="iconCls:'icon-search'" style="padding:0px;">
+    <ul class="easyui-tree">
+    <li data-options="iconCls:'icon-surat_keputusan'">
+    <a href="<?php echo base_url();?>index.php/pembelian">Pembelian</a>
+    </li>
+    <li data-options="iconCls:'icon-surat_keluar'">
+    <a href="<?php echo base_url();?>index.php/penjualan">Penjualan</a>
+    </li>
+    </ul>
+    </div>
+</div>
+<div title="Laporan" data-options="iconCls:'icon-print'" style="overflow:auto;padding:5px 0px;">
+    <div title="TreeMenu" data-options="iconCls:'icon-search'" style="padding:0px;">
+    <ul class="easyui-tree">
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/lap_barang">Stok Barang</a></span>
+    </li>
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/lap_beli">Pembelian</a></span>
+    </li>
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/lap_jual">Penjualan</a></span>
+    </li>
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/profit">Profit</a></span>
+    </li>
+    </ul>
+    </div>
+</div>
+<div title="Grafik" data-options="iconCls:'icon-grafik'" style="overflow:auto;padding:5px 0px;">
+    <div title="TreeMenu" data-options="iconCls:'icon-search'" style="padding:0px;">
+    <ul class="easyui-tree">
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/grafik_beli">Pembelian</a></span>
+    </li>
+    <li>
+    <span><a href="<?php echo base_url();?>index.php/grafik_jual">Penjualan</a></span>
+    </li>
+	 <li>
+    <span><a href="<?php echo base_url();?>index.php/grafik_pie">PIE</a></span>
+    </li>
+    </ul>
+    </div>
+</div>
---
+++ application/views/menu_user.php
@@ -0,0 +1,12 @@
+<div title="Transaksi" data-options="iconCls:'icon-tip'" style="overflow:auto;padding:5px 0px;">
+    <div title="TreeMenu" data-options="iconCls:'icon-search'" style="padding:0px;">
+    <ul class="easyui-tree">
+    <li data-options="iconCls:'icon-surat_keputusan'">
+    <a href="<?php echo base_url();?>index.php/pembelian">Pembelian</a>
+    </li>
+    <li data-options="iconCls:'icon-surat_keluar'">
+    <a href="<?php echo base_url();?>index.php/penjualan">Penjualan</a>
+    </li>
+    </ul>
+    </div>
+</div>
